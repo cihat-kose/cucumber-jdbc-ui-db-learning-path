fb183ceb0c9b4cb04c28f8bce558de85facd5c4b
Cihat Köse <cihat.kose@hotmail.com>
2025-06-07 00:03:04 +0200

    Expand prerequisites section with additional tools

--- src/test/java/apachePOI/_09_ReadExcelColumnsToList.java
+++ src/test/java/apachePOI/_09_ReadExcelColumnsToList.java
@@ -18,7 +18,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class _09_Question_03 {
+public class _09_ReadExcelColumnsToList {
 
     public static void main(String[] args) {
         String path = "src/test/java/ApachePOI/resource/ApacheExcel.xlsx";
